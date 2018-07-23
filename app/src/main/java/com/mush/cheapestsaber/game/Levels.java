6ed77b964b37ab7656e250533f43938d596e2d92
the-cic <the-cic@github.com>
2018-07-23 20:56:07 +0200

    ~ loading level info from level files

--- app/src/main/java/com/mush/cheapestsaber/game/Levels.java
+++ app/src/main/java/com/mush/cheapestsaber/game/Levels.java
@@ -1,6 +1,7 @@
 package com.mush.cheapestsaber.game;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.util.Log;
 
 import java.io.BufferedReader;
@@ -8,6 +9,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -20,12 +22,8 @@ public class Levels {
         public String title;
         public String[] difficultyNames;
 
-        public Level(String line) {
-            String[] parts = line.split(":");
-            fileName = parts[0].trim();
-            title = parts[1].trim();
-            String difficulty = parts[2].trim();
-            difficultyNames = difficulty.split("[ ,]+");
+        public boolean hasAllFields() {
+            return fileName != null && title != null && difficultyNames != null;
         }
     }
 
@@ -33,16 +31,19 @@ public class Levels {
     private static final String fileName = "index";
 
     public List<Level> levels;
+    private Context context;
 
     public Levels(Context context) {
+        this.context = context;
+
         InputStream inputStream = context.getResources().openRawResource(
                 context.getResources().getIdentifier(fileName,
                         "raw", context.getPackageName()));
 
-        readFile(inputStream);
+        readIndexFile(inputStream);
     }
 
-    private void readFile(InputStream inputStream) {
+    private void readIndexFile(InputStream inputStream) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         String line;
         levels = new ArrayList<>();
@@ -51,9 +52,14 @@ public class Levels {
             while (( line = reader.readLine()) != null) {
                 line = line.trim();
                 if (!line.isEmpty()) {
-                    Level level = new Level(line);
-                    levels.add(level);
-                    Log.i(TAG, "" + line);
+                    Level level = readLevelFile(line);
+                    if (level != null) {
+                        levels.add(level);
+                        Log.i(TAG, level.fileName);
+                        Log.i(TAG, level.title);
+                        Log.i(TAG, Arrays.toString(level.difficultyNames));
+
+                    }
                 }
             }
         } catch (IOException e) {
@@ -61,4 +67,46 @@ public class Levels {
         }
     }
 
+    private Level readLevelFile(String levelFileName) {
+        InputStream inputStream = context.getResources().openRawResource(
+                context.getResources().getIdentifier(levelFileName,
+                        "raw", context.getPackageName()));
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        Level level = new Level();
+        level.fileName = levelFileName;
+
+        String line;
+        try {
+            while (( line = reader.readLine()) != null && !level.hasAllFields()) {
+                line = line.trim();
+                if (!line.isEmpty()) {
+                    String parts[] = line.split("[\t ,]+");
+                    switch (parts[0]) {
+                        case "title":
+                            level.title = TextUtils.join(" ", getLineParams(parts));
+                            break;
+                        case "difficulty":
+                            level.difficultyNames = getLineParams(parts);
+                            break;
+                    }
+                }
+            }
+
+            return level;
+
+        } catch (IOException e) {
+            Log.e(TAG, "" + e);
+            return null;
+        }
+    }
+
+    private String[] getLineParams(String[] line) {
+        String[] params = new String[line.length - 1];
+        if (params.length > 0) {
+            System.arraycopy(line, 1, params, 0, params.length);
+        }
+        return params;
+    }
 }
